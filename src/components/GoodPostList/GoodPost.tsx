2225724350925ebfb20190a699d48074a391fd18
minji kim <kmj_1027@naver.com>
2024-05-07 19:46:27 +0900

    ✨ feat(goodpost): create goodpost component and make temporary layout
    create goodpost component and make temporary layout

--- src/components/GoodPostList/GoodPost.tsx
+++ src/components/GoodPostList/GoodPost.tsx
@@ -2,9 +2,14 @@ import PostProfile from "../Common/PostProfile";
 
 const GoodPost = () => {
     return (
-        <>
+        <div className="p-6 bg-white">
             <PostProfile url="" name="minjijijijiral" />
-        </>
+            <div className="my-4 text-left">
+                {
+                    "안녕안녕 나는 지수야 헬륨가스 먹었더니 요렇게 됐지!!! 이바부야 ~ >_< 안녕하세요 ~ 처음 만난 사람들도 안녕하세요. 워어 안녕안녕안녕 안녕하세요 처음만난사람들도 안녕하세요 짝짞잒 췌 췌 췌키라웃"
+                }
+            </div>
+        </div>
     );
 };
 
