79438a18a50d0cc7f0c8c3f74774450b98260892
minji kim <kmj_1027@naver.com>
2024-05-10 00:01:49 +0900

    ✨ feat(goodpost): 선행게시판 리스트 레이아웃
    선행게시판 리스트 레이아웃

--- src/components/GoodPostList/GoodPost.tsx
+++ src/components/GoodPostList/GoodPost.tsx
@@ -4,14 +4,14 @@ import PostProfile from "../Common/PostProfile";
 
 const GoodPost = () => {
     return (
-        <div className="p-6 bg-white">
+        <div className="p-6 border-b-[1px]">
             <PostProfile url="" name="minjijijijiral" />
             <div className="my-4 text-left">
                 {
                     "안녕안녕 나는 지수야 헬륨가스 먹었더니 요렇게 됐지!!! 이바부야 ~ >_< 안녕하세요 ~ 처음 만난 사람들도 안녕하세요. 워어 안녕안녕안녕 안녕하세요 처음만난사람들도 안녕하세요 짝짞잒 췌 췌 췌키라웃"
                 }
             </div>
-            <div className="flex">
+            <div className="flex gap-4">
                 <LikeIcon />
                 <CommentIcon />
             </div>
--- src/pages/GoodPostList.tsx
+++ src/pages/GoodPostList.tsx
@@ -2,9 +2,23 @@ import GoodPost from "../components/GoodPostList/GoodPost";
 
 const GoodPostList = () => {
     return (
-        <>
+        <div className=" h-[calc(100vh-136px)] mb-7 overflow-scroll rounded-lg border-[1px] bg-white">
             <GoodPost />
-        </>
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+            <GoodPost />
+        </div>
     );
 };
 
