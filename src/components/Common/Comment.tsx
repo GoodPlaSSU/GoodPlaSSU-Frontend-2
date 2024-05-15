b418a8a446f4e8244fd1f96491a6e258c22acf3c
minji kim <kmj_1027@naver.com>
2024-05-16 00:57:03 +0900

    ✨ feat(postdetail): 글, 댓글 css layout 개발
    글, 댓글 css layout 개발

---
+++ src/components/Common/Comment.tsx
@@ -0,0 +1,31 @@
+import ProfileImage from "./ProfileImage";
+
+type CommentType = {
+    profile: string,
+    name: string,
+    date: string,
+    content: string
+}
+
+const Comment = ({profile, name, date, content}: CommentType) => {
+    return (
+        <div className="p-3 bg-[#f5f5f5] rounded-xl">
+            <div className="flex items-center">
+                <ProfileImage url={profile} size="20" />
+                <div>
+                    <div className="pl-2 text-bold text-sm text-primary">
+                        {name}
+                    </div>
+                </div>
+                <div className="ml-auto text-medium text-xs text-title">
+                    {date}
+                </div>
+            </div>
+            <div className="pl-7 text-base text-semibold text-text text-left">
+                {content}
+            </div>
+        </div>
+    );
+};
+
+export default Comment;
--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -1,12 +1,33 @@
+import Comment from "../components/Common/Comment";
+import LikeIcon from "../components/Common/LikeIcon";
+import PostProfile from "../components/Common/PostProfile";
+
 const PostDetail = () => {
     return (
-        <div className="h-[calc(100vh-136px)] mb-7 overflow-scroll rounded-lg border-[1px] bg-white">
-            df
+        <div className="p-6">
+            <div className="mb-6">
+                <div className="flex items-center">
+                    <PostProfile url="" name="minji" />
+                    <span className="ml-auto text-xs text-title">
+                        {"2024.01.02"}
+                    </span>
+                </div>
+                <div className="my-4 text-left">{"sdfdsfdsfsdfs"}</div>
+                <LikeIcon isOn={false} count={12312} />
+            </div>
+            <div>
+                <div className="mb-4 text-left font-semibold text-primary">
+                    {"comment:"}
+                </div>
+                <Comment
+                    profile=""
+                    name="Minji Kim"
+                    date="2024.10.27"
+                    content="sfsdf"
+                />
+            </div>
         </div>
     );
-    
-}
+};
 
-
-
-export default PostDetail
+export default PostDetail;
