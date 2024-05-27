8fe4b1ed47d7e656f64206060881aa158d6f1266
minji kim <kmj_1027@naver.com>
2024-05-27 19:48:45 +0900

    ✨ feat(postdetail): 데이터 불러오기 성공
    데이터 불러오기 성공

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -9,12 +9,14 @@ import CommentWrite from "../components/PostDetail/CommentWrite";
 
 type Data = {
     id: number;
-    cheer_count: number;
-    imagesResponse: { imageUrls: string[] };
+    like_count: number;
+    images: { imageUrls: string[] };
+    content: string;
     updated_at: string;
     writer_id: number;
-    writer_portrait: string;
+    writer_profile: string;
     writer_name: string;
+    comments: string[];
 };
 
 const PostDetail = () => {
@@ -36,32 +38,32 @@ const PostDetail = () => {
     });
 
     return (
-        <div className="w-full h-full p-6 overflow-scroll" ref={detailRef}>
-            <div className="mb-6">
-                <div className="flex items-center">
-                    <PostProfile url={""} name={""} />
-                    <span className="ml-auto text-xs text-title">
-                        {"data!.updated_at.substring(0, 10)"}
-                    </span>
+        <div className="w-full h-full overflow-scroll" ref={detailRef}>
+                <div className="mb-6">
+                    <div className="flex items-center">
+                        <PostProfile url={""} name={""} />
+                        <span className="ml-auto text-xs text-title">
+                            {"data!.updated_at.substring(0, 10)"}
+                        </span>
+                    </div>
+                    <div className="my-4 text-left">{"sdfdsfdsfsdfs"}</div>
+                    <LikeIcon isOn={false} count={0} />
                 </div>
-                <div className="my-4 text-left">{"sdfdsfdsfsdfs"}</div>
-                <LikeIcon isOn={false} count={0} />
-            </div>
-            <div>
-                <div className="mb-4 font-semibold text-left text-primary">
-                    {"comment"}
+                <div>
+                    <div className="mb-4 font-semibold text-left text-primary">
+                        {"comment"}
+                    </div>
+                    {!isPending &&
+                        comments.map((e, i) => (
+                            <Comment
+                                key={i}
+                                profile=""
+                                name="Minji Kim"
+                                date="2024.10.27"
+                                content="sfsdf"
+                            />
+                        ))}
                 </div>
-                {!isPending &&
-                    comments.map((e, i) => (
-                        <Comment
-                            key={i}
-                            profile=""
-                            name="Minji Kim"
-                            date="2024.10.27"
-                            content="sfsdf"
-                        />
-                    ))}
-            </div>
             <CommentWrite scrollHandler={scrollToBottom} />
         </div>
     );
